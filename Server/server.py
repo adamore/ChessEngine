c686e29f90e76f9e650b022837e531934b1103b0
Anthony Damore <anthonym.damore@gmail.com>
2020-12-19 14:35:22 -0800

    Changed requirments

--- Server/server.py
+++ Server/server.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 app = Api(app)
 
 app.add_resource(GameManager, "/make-white-move")
-app.add_resource(GameCreator, "/make-white-move")
+app.add_resource(GameCreator, "/create-game")
 
 
 if __name__ == "__main__":
