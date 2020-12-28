c9c381e61ab0b3b2278cb34bc55e604fcd730dd5
Anthony Damore <anthonym.damore@gmail.com>
2020-12-28 15:17:29 -0800

    Fixdc error on promotion

--- Engine/Engine.py
+++ Engine/Engine.py
@@ -59,16 +59,14 @@ SQUARE_VALUES = {
      3, -14, -50, -57, -18, 13, 4, 17, 30, -3, -14, 6, -1, 40, 18),
 }
 SQUARES = np.array([
-    chess.A1, chess.B1, chess.C1, chess.D1, chess.E1, chess.F1, chess.G1,
-    chess.H1, chess.A2, chess.B2, chess.C2, chess.D2, chess.E2, chess.F2,
-    chess.G2, chess.H2, chess.A3, chess.B3, chess.C3, chess.D3, chess.E3,
-    chess.F3, chess.G3, chess.H3, chess.A4, chess.B4, chess.C4, chess.D4,
-    chess.E4, chess.F4, chess.G4, chess.H4, chess.A5, chess.B5, chess.C5,
-    chess.D5, chess.E5, chess.F5, chess.G5, chess.H5, chess.A6, chess.B6,
-    chess.C6, chess.D6, chess.E6, chess.F6, chess.G6, chess.H6, chess.A7,
-    chess.B7, chess.C7, chess.D7, chess.E7, chess.F7, chess.G7, chess.H7,
-    chess.A8, chess.B8, chess.C8, chess.D8, chess.E8, chess.F8, chess.G8,
-    chess.H8
+    chess.A1, chess.B1, chess.C1, chess.D1, chess.E1, chess.F1, chess.G1, chess.H1, 
+    chess.A2, chess.B2, chess.C2, chess.D2, chess.E2, chess.F2, chess.G2, chess.H2, 
+    chess.A3, chess.B3, chess.C3, chess.D3, chess.E3, chess.F3, chess.G3, chess.H3, 
+    chess.A4, chess.B4, chess.C4, chess.D4, chess.E4, chess.F4, chess.G4, chess.H4, 
+    chess.A5, chess.B5, chess.C5, chess.D5, chess.E5, chess.F5, chess.G5, chess.H5, 
+    chess.A6, chess.B6, chess.C6, chess.D6, chess.E6, chess.F6, chess.G6, chess.H6, 
+    chess.A7, chess.B7, chess.C7, chess.D7, chess.E7, chess.F7, chess.G7, chess.H7,
+    chess.A8, chess.B8, chess.C8, chess.D8, chess.E8, chess.F8, chess.G8, chess.H8
 ])
 
 
@@ -250,9 +248,13 @@ class MoveTree:
     def takeTurn(self, move):
         if isinstance(move, str):
             chessMove = chess.Move.from_uci(move)
+            pieceMoved = self.board.piece_at(chessMove.from_square)
+            if pieceMoved.piece_type == chess.PAWN 
+                and (pieceMoved.to_square in SQUARES[0:8] 
+                or pieceMoved.to_square in SQUARES[56:64]):
+                chessMove.promotion = chess.QUEEN
             if chessMove not in self.board.legal_moves:
                 raise AssertionError
-
             self.board.push(chessMove)
             return
         elif isinstance(move, chess.Move):
